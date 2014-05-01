240179e9a4ac8f95988d770fbabeb698638defc7
Anders Hafreager <anders@hafreager.no>
2014-05-01 17:09:39 +0200

    Added working forces test for lennard jones.

--- tests/forces.cpp
+++ tests/forces.cpp
@@ -4,31 +4,19 @@ using std::vector;
 
 #include <UnitTest++.h>
 #include <potentials/lennardjonespotential.h>
-#include <atom.h>
-#include <atomtype.h>
-#include <system.h>
+#include <includes.h>
 
 SUITE(Forces) {
     TEST(ArgonForceTest) {
-//        Atom atom1(AtomType::atomTypeFromAtomType(AtomTypes::Argon));
-//        Atom atom2(AtomType::atomTypeFromAtomType(AtomTypes::Argon));
-//        atom1.position[0] = 0;
-//        atom2.position[0] = 0;
+        Atom atom1(AtomType::atomTypeFromAtomType(AtomTypes::Argon));
+        Atom atom2(AtomType::atomTypeFromAtomType(AtomTypes::Argon));
 
-//        System system;
-//        int nodeIndex = 0;
-//        vector<int> numNodesVector(3,1);
-//        vector<double> systemLength(3,1);
-//        double cutOffDistance = 1.0;
-//        system.initialize(nodeIndex, numNodesVector, systemLength, cutOffDistance);
-//        system.atoms().push_back(atom1);
-//        system.atoms().push_back(atom2);
-//        system.updateCells();
+        atom1.setPosition(1, 0, 0);
 
-//        LennardJonesPotential potential;
-//        potential.calculateForces(system);
-//        CHECK_EQUAL(atom1.force[0],-atom2.force[0]);
-//        CHECK_EQUAL(atom1.force[1],-atom2.force[1]);
-//        CHECK_EQUAL(atom1.force[2],-atom2.force[2]);
+        LennardJonesPotential potential;
+        potential.twoParticleAction(&atom1,&atom2);
+        CHECK_EQUAL(atom1.force[0], -atom2.force[0]);
+        CHECK_EQUAL(atom1.force[1], -atom2.force[1]);
+        CHECK_EQUAL(atom1.force[2], -atom2.force[2]);
     }
 }
