ac2b60dde66741e8844ad06d3c6db90c56f72b5d
Anders Hafreager <anders@hafreager.no>
2014-05-01 17:10:29 +0200

    Nothing important.

--- example/main.cpp
+++ example/main.cpp
@@ -8,20 +8,21 @@ int main()
 {
     Random::setSeed(2);
     Simulator simulator;
-    vector<double> systemLength(3,10);
+    vector<double> systemLength(3,1);
     simulator.initialize(0, vector<int>(3,1), systemLength);
-    LennardJonesPotential *potential = (LennardJonesPotential*)simulator.system().addPotential(PotentialType::LennardJones);
+    // LennardJonesPotential *potential = (LennardJonesPotential*)simulator.system().addPotential(PotentialType::LennardJones);
 
     // Generator::generateFCC(simulator.system(), 1, vector<int>(3,1), AtomType::atomTypeFromAtomType(AtomTypes::Argon));
     Atom &atom1 = simulator.system().addAtom();
-    Atom &atom2 = simulator.system().addAtom();
+    // Atom &atom2 = simulator.system().addAtom();
     atom1.setType(AtomType::atomTypeFromAtomType(AtomTypes::Argon));
-    atom2.setType(AtomType::atomTypeFromAtomType(AtomTypes::Argon));
+    // atom2.setType(AtomType::atomTypeFromAtomType(AtomTypes::Argon));
 
-    atom1.setPosition(2,0,0);
+    atom1.setPosition(0.5,0.5,0.5);
+    cout << "Things: " << simulator.system().atomManager() << endl;
 
     cout << "Before step: " << simulator.system().atomManager() << endl;
-    simulator.step();
+    // simulator.step();
     cout << "After step: " << simulator.system().atomManager() << endl;
 
     return 0;
