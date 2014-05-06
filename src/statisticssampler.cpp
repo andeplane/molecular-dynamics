de0b91f15c5aeddddfb76fb81f5c7507ce1ee519
Anders Hafreager <anders@hafreager.no>
2014-05-06 02:01:36 +0200

    Added simple measurements of energy and momentum.

--- src/statisticssampler.cpp
+++ src/statisticssampler.cpp
@@ -1,5 +1,48 @@
-#include "statisticssampler.h"
+#include <statisticssampler.h>
+#include <system.h>
+#include <atom.h>
+#include <atomtype.h>
+#include <potentials/potential.h>
 
 StatisticsSampler::StatisticsSampler()
 {
+
+}
+
+double StatisticsSampler::calculateKineticEnergy(System &system)
+{
+    double kineticEnergy = 0;
+    system.atomManager().atoms().iterate([&](Atom &atom) {
+        kineticEnergy += 0.5*atom.type()->mass()*(atom.velocity[0]*atom.velocity[0] + atom.velocity[1]*atom.velocity[1] + atom.velocity[2]*atom.velocity[2]);
+    });
+
+    return kineticEnergy;
+}
+
+double StatisticsSampler::calculatePotentialEnergy(System &system)
+{
+    double potentialEnergy = 0;
+    for(Potential *potential : system.potentials()) {
+        potentialEnergy += potential->potentialEnergy();
+    }
+
+    return potentialEnergy;
+}
+
+double StatisticsSampler::calculateTotalEnergy(System &system)
+{
+    return calculatePotentialEnergy(system) + calculateKineticEnergy(system);
+}
+
+vector<double> StatisticsSampler::calculateTotalMomentum(System &system)
+{
+    vector<double> momentum(3,0);
+
+    system.atomManager().atoms().iterate([&](Atom &atom) {
+        momentum[0] += atom.velocity[0]*atom.type()->mass();
+        momentum[1] += atom.velocity[1]*atom.type()->mass();
+        momentum[2] += atom.velocity[2]*atom.type()->mass();
+    });
+
+    return momentum;
 }
--- src/statisticssampler.h
+++ src/statisticssampler.h
@@ -1,10 +1,17 @@
 #ifndef STATISTICSSAMPLER_H
 #define STATISTICSSAMPLER_H
+#include <vector>
+using std::vector;
 
+class System;
 class StatisticsSampler
 {
 public:
     StatisticsSampler();
+    double calculateKineticEnergy(System &system);
+    double calculatePotentialEnergy(System &system);
+    double calculateTotalEnergy(System &system);
+    vector<double> calculateTotalMomentum(System &system);
 };
 
 #endif // STATISTICSSAMPLER_H
