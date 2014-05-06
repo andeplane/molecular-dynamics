a9b354dfe5ef79e52754bca31d0685f7469e7da3
Anders Hafreager <anders@hafreager.no>
2014-05-06 02:01:13 +0200

    Moved the reset velocity outside the for loop.

--- src/generator.cpp
+++ src/generator.cpp
@@ -6,7 +6,6 @@
 void Generator::generateFCC(System &system, double unitCellLength, vector<int> numberOfUnitCells, AtomType *atomType, double temperature)
 {
     system.removeAllAtoms();
-    cout << "Creating fcc, inverse mass: " << atomType->massInverse() << endl;
 
     vector<double> systemLength(3,0);
     systemLength[0] = numberOfUnitCells[0]*unitCellLength;
@@ -22,15 +21,15 @@ void Generator::generateFCC(System &system, double unitCellLength, vector<int> n
         for(int y = 0; y < numberOfUnitCells[1]; y++) {
             for(int z = 0; z < numberOfUnitCells[2]; z++) {
                 for(int k = 0; k < 4; k++) {
-                    // Set positions and type
                     Atom &atom = system.addAtom(atomType);
                     atom.position[0] = (x+xCell[k]) * unitCellLength;
                     atom.position[1] = (y+yCell[k]) * unitCellLength;
                     atom.position[2] = (z+zCell[k]) * unitCellLength;
                     atom.setId(atomID++);
-                    atom.resetMaxwellianVelocity(temperature);
                 }
             }
         }
     }
+
+    system.atomManager().atoms().resetVelocityMaxwellian(temperature);
 }
