0f448c5a1b82f47b25d5be54bde8fb01f0e40e6b
Anders Hafreager <anders@hafreager.no>
2014-05-10 15:06:27 +0200

    Working on iterator. It iterates better than before by also adding ghosts to other ghosts neighbor lists.

--- example/main.cpp
+++ example/main.cpp
@@ -11,12 +11,9 @@ int main()
 {
     Random::setSeed(1);
     Simulator simulator;
-    int numberOfTimesteps = 1000;
-    double systemSize = 100;
-    double delta = systemSize / 2;
-    vector<double> systemLength(3,UnitConverter::lengthFromAngstroms(systemSize));
+    int numberOfTimesteps = 50;
 
-    simulator.initialize(0, vector<int>(3,1), systemLength);
+    simulator.initialize(0, vector<int>(3,1), UnitConverter::lengthFromAngstroms({10, 10, 10}));
     simulator.setTimestep(UnitConverter::timeFromSI(1e-15));
 
     USCSIO2Potential *potential = (USCSIO2Potential*)simulator.system().addPotential(PotentialType::USCSilica);
@@ -24,50 +21,50 @@ int main()
     FileManager fileManager;
     // fileManager.loadMts0("/projects/andershaf_nanoporous_sio2_compressed_pore/test/heat/initial-crystal/mts0",{1,1,1},simulator.system());
 
-    Atom &silicon = simulator.system().addAtom(AtomType::atomTypeFromAtomType(AtomTypes::Silicon));
-    silicon.setPosition(UnitConverter::lengthFromAngstroms({1.825+delta, 1.826+delta, 1.826+delta}));
-
-    Atom &oxygen1 = simulator.system().addAtom(AtomType::atomTypeFromAtomType(AtomTypes::Oxygen));
-    oxygen1.setPosition(UnitConverter::lengthFromAngstroms({0.896+delta,0.895+delta, 0.896+delta}));
-
-    Atom &oxygen2 = simulator.system().addAtom(AtomType::atomTypeFromAtomType(AtomTypes::Oxygen));
-    oxygen2.setPosition(UnitConverter::lengthFromAngstroms({1.146+delta, 3.150+delta, 2.435+delta}));
-
-    Atom &oxygen3 = simulator.system().addAtom(AtomType::atomTypeFromAtomType(AtomTypes::Oxygen));
-    oxygen3.setPosition(UnitConverter::lengthFromAngstroms({2.434+delta, 1.146+delta, 3.150+delta}));
-
-    Atom &oxygen4 = simulator.system().addAtom(AtomType::atomTypeFromAtomType(AtomTypes::Oxygen));
-    oxygen4.setPosition(UnitConverter::lengthFromAngstroms({3.150+delta, 2.434+delta, 1.145+delta}));
-
-    simulator.system().atomManager().atoms().resetVelocityMaxwellian(UnitConverter::temperatureFromSI(0));
-    simulator.system().atomManager().setGhostAtomsEnabled(false);
-    cout << simulator.system() << endl;
     simulator.system().removeTotalMomentum();
+
+    Generator::addSiO4Molecule(simulator.system(), {5,5,5});
+    simulator.system().atomManager().setCutoffDistance(INFINITY);
+    cout << simulator.system().atomManager() << endl;
+    exit(1);
+//    simulator.system().atomManager().atoms().iterate([](Atom &atom) {
+//        atom.addVelocity(-5e-4,2e-3,1e-4);
+//    });
+    cout << "Before adding: " << simulator.system().atomManager().atoms() << endl << endl;
+    vector<double> systemLength = simulator.system().systemLength();
+    simulator.system().atomManager().atoms().iterate([&](Atom &atom) {
+        for(int i=-1; i<=1; i++) {
+            for(int j=-1; j<=1; j++) {
+                for(int k=-1; k<=1; k++) {
+                    if(i == 0 && j == 0 && k == 0) continue;
+                    double x = atom.position[0] + i*systemLength[0];
+                    double y = atom.position[1] + j*systemLength[1];
+                    double z = atom.position[2] + k*systemLength[2];
+                    simulator.system().addAtom(atom.type(), {x,y,z});
+                }
+            }
+        }
+    });
+
+    cout << "After adding: " << simulator.system().atomManager().atoms() << endl << endl;
+
+    exit(1);
+
     cout << simulator.system() << endl;
-//    cout << "Before: " << endl << simulator.system().atomManager()<< endl;
-
-//    simulator.step();
-
-//    cout << "After: " << endl << simulator.system().atomManager()<< endl;
-//    fileManager.saveMovieFrame(simulator.system().atomManager().atoms().atoms(),simulator.system().topology());
-//    fileManager.finalize();
-//    return 0;
-
     for(int timestep=0; timestep<numberOfTimesteps; timestep++) {
         fileManager.saveMovieFrame(simulator.system().atomManager().atoms().atoms(),simulator.system().topology());
-        cout << "Momentum in " << timestep << ": " << simulator.sampler().calculateTotalMomentum(simulator.system()) << endl;
+
+        if(timestep % 10 == 0) {
+            // cout << timestep << "..";
+            cout << timestep << " momentum: " << simulator.sampler().calculateTotalMomentum(simulator.system()) << endl;
+        }
+
+        fflush(stdout);
+
         simulator.step();
-
-//        if(timestep%100 == 0) {
-//            cout << "Timestep " << timestep;
-//            cout << "   Total energy: " << simulator.sampler().calculateTotalEnergy(simulator.system());
-//            cout << " (K=" << simulator.sampler().calculateKineticEnergy(simulator.system()) << "  P=" << simulator.sampler().calculatePotentialEnergy(simulator.system()) << ")" << endl;
-//        } else {
-//            cout << "Timestep " << timestep << endl;
-//        }
-
     }
     cout << "Successfully computed " << numberOfTimesteps << " timesteps." << endl;
+    cout << simulator.system().atomManager().atoms() << endl;
     fileManager.saveMovieFrame(simulator.system().atomManager().atoms().atoms(),simulator.system().topology());
     fileManager.finalize();
     return 0;
--- src/atomiterators/atomiteratordefault.cpp
+++ src/atomiterators/atomiteratordefault.cpp
@@ -40,32 +40,41 @@ void AtomIteratorDefault::iterate(AtomManager &atomManager) {
     CellData &cellData = atomManager.cellData();
     vector<Cell> &cells = cellData.cells;
     double maximumNeighborDistanceSquared = m_maximumNeighborDistance*m_maximumNeighborDistance;
+//    cout << endl << endl << endl << "Will iterate through all atoms. List:" << atomManager << endl << endl;
+//    cout << "Max neighbor distance: " << UnitConverter::lengthToAngstroms(m_maximumNeighborDistance) << endl;
 
-    for(int cellX=1; cellX<=cellData.numberOfCellsWithoutGhostCells[0]; cellX++) {
-        for(int cellY=1; cellY<=cellData.numberOfCellsWithoutGhostCells[1]; cellY++) {
-            for(int cellZ=1; cellZ<=cellData.numberOfCellsWithoutGhostCells[2]; cellZ++) {
+    for(int cellX=0; cellX<cellData.numberOfCellsWithGhostCells[0]; cellX++) {
+        for(int cellY=0; cellY<cellData.numberOfCellsWithGhostCells[1]; cellY++) {
+            for(int cellZ=0; cellZ<cellData.numberOfCellsWithGhostCells[2]; cellZ++) {
                 Cell &cell1 = safeOrQuickVectorLookup(cells,Cell::cellIndexFromIJK(cellX, cellY, cellZ, cellData));
 
                 for(int cell2X=cellX-1; cell2X<=cellX+1; cell2X++) {
                     for(int cell2Y=cellY-1; cell2Y<=cellY+1; cell2Y++) {
                         for(int cell2Z=cellZ-1; cell2Z<=cellZ+1; cell2Z++) {
-                            Cell &cell2 = safeOrQuickVectorLookup(cells,Cell::cellIndexFromIJK(cell2X, cell2Y, cell2Z, cellData));
+
+                            int cell2XPeriodic = (cell2X + cellData.numberOfCellsWithGhostCells[0]) % cellData.numberOfCellsWithGhostCells[0];
+                            int cell2YPeriodic = (cell2Y + cellData.numberOfCellsWithGhostCells[1]) % cellData.numberOfCellsWithGhostCells[1];
+                            int cell2ZPeriodic = (cell2Z + cellData.numberOfCellsWithGhostCells[2]) % cellData.numberOfCellsWithGhostCells[2];
+                            Cell &cell2 = safeOrQuickVectorLookup(cells,Cell::cellIndexFromIJK(cell2XPeriodic, cell2YPeriodic, cell2ZPeriodic, cellData));
 
                             for(Atom *atom1 : cell1.atoms()) {
                                 for(Atom *atom2 : cell2.atoms()) {
-                                    if(m_createNeighborList && (atom1->uniqueId() < atom2->uniqueId() || atom2->ghost())) {
+                                    // cout << "Pair: (" << atom1->uniqueId() << (atom1->ghost() ? "*" : "") << "," << atom2->uniqueId() << (atom2->ghost() ? "*" : "") << ")" << endl;
+                                    if(m_createNeighborList && atom1->uniqueId() < atom2->uniqueId()) {
                                         double dx = atom1->position[0] - atom2->position[0];
                                         double dy = atom1->position[1] - atom2->position[1];
                                         double dz = atom1->position[2] - atom2->position[2];
                                         double dr2 = dx*dx + dy*dy + dz*dz;
 
                                         if(dr2 < maximumNeighborDistanceSquared) {
+                                            // cout << "Added pair: (" << atom1->uniqueId() << (atom1->ghost() ? "*" : "") << "," << atom2->uniqueId() << (atom2->ghost() ? "*" : "") << ")" << endl;
                                             atom1->addNeighbor(*atom2);
                                             atom2->addNeighbor(*atom1);
                                         }
                                     }
 
-                                    if(atom1->originalUniqueId() <= atom2->originalUniqueId() && !atom2->ghost()) continue; // Newton's 3rd law, always calculate if atom2 is ghost
+                                    // Skip two particle forces between ghosts. Also skip two particle force between non ghosts unless atom1.uniqueId<atom2.uniqueId (the other permutation will compute forces)
+                                    if( (atom1->ghost() && atom2->ghost()) || (atom1->uniqueId() <= atom2->uniqueId() && (!atom1->ghost() && !atom2->ghost()) )) continue;
                                     m_twoParticleAction(atom1,atom2);
                                 }
                             } // Loop atoms
@@ -78,17 +87,27 @@ void AtomIteratorDefault::iterate(AtomManager &atomManager) {
 
     if(!m_threeParticleAction) return;
     // Three particle loop
-    atomManager.atoms().iterate([&](Atom &atom) {
+
+//    atomManager.atoms().iterate([] (Atom &atom) {
+//       cout << "Neighbors " << atom.uniqueId() << ": " << atom.neighbors() << endl;
+//    });
+
+    auto threeParticleLooper = [&](Atom &atom) {
         Atom *atom1 = &atom;
+//        cout << endl << endl << "Num neighbors = " << atom1->neighbors().size() << " for " << atom << endl;
+//        cout << "Neighbors: " << atom1->neighbors() << endl;
         vector<atomUniqueId> &neighbors = atom1->neighbors();
         if(neighbors.size() < 2) return; // No three particle contribution here
 
         unsigned long atom1OriginalUniqueId = atom1->originalUniqueId();
+        // cout << "Singlet: " << "(" << atom1OriginalUniqueId << (atom1->ghost() ? "*" : "") << ")  --- unique id (" << atom1->uniqueId() << (atom1->ghost() ? "*" : "") << ")" << endl;
+
         for(unsigned long neighborIndex1 = 0; neighborIndex1<neighbors.size()-1; neighborIndex1++) {
             atomUniqueId &neighbor1UniqueId = safeOrQuickVectorLookup(neighbors, neighborIndex1);
             Atom *atom2 = &atomManager.getAtomByUniqueId(neighbor1UniqueId);
 
             unsigned long atom2OriginalUniqueId = atom2->originalUniqueId();
+            // cout << "Pair: (" << atom1OriginalUniqueId << (atom1->ghost() ? "*" : "") << "," << atom2OriginalUniqueId << (atom2->ghost() ? "*" : "") << ")" << "  --- unique ids (" << atom1->uniqueId() << (atom1->ghost() ? "*" : "") << "," << atom2->uniqueId() << (atom2->ghost() ? "*" : "") << ")" << endl;
             if(atom2OriginalUniqueId < atom1OriginalUniqueId) continue; // Only accept configurations where the original atom ids are in ascending order
 
             for(unsigned long neighborIndex2 = neighborIndex1+1; neighborIndex2<neighbors.size(); neighborIndex2++) {
@@ -96,10 +115,42 @@ void AtomIteratorDefault::iterate(AtomManager &atomManager) {
                 Atom *atom3 = &atomManager.getAtomByUniqueId(neighbor2UniqueId);
 
                 unsigned long atom3OriginalUniqueId = atom3->originalUniqueId();
+                // cout << "Triplet: (" << atom1OriginalUniqueId << (atom1->ghost() ? "*" : "") << "," << atom2OriginalUniqueId << (atom2->ghost() ? "*" : "") << "," << atom3OriginalUniqueId << (atom3->ghost() ? "*" : "") << ")  --- unique ids (" << atom1->uniqueId() << (atom1->ghost() ? "*" : "") << "," << atom2->uniqueId() << (atom2->ghost() ? "*" : "") << ", " << atom3->uniqueId() << (atom3->ghost() ? "*" : "") << ")" << endl;
                 if(atom3OriginalUniqueId < atom2OriginalUniqueId) continue;  // Only accept configurations where the original atom ids are in ascending order
+                // cout << "Executing three particle action with ";
+                // cout << " triplet: (" << atom1OriginalUniqueId << (atom1->ghost() ? "*" : "") << "," << atom2OriginalUniqueId << (atom2->ghost() ? "*" : "") << "," << atom3OriginalUniqueId << (atom3->ghost() ? "*" : "") << ")  --- unique ids (" << atom1->uniqueId() << (atom1->ghost() ? "*" : "") << "," << atom2->uniqueId() << (atom2->ghost() ? "*" : "") << ", " << atom3->uniqueId() << (atom3->ghost() ? "*" : "") << ")" << endl;
+
+//                cout << *atom1 << endl;
+//                cout << *atom2 << endl;
+//                cout << *atom3 << endl;
+                if(atom1->ghost() && atom2->ghost() && atom3->ghost()) continue;
 
                 m_threeParticleAction(atom1,atom2,atom3);
             }
         }
+    };
+
+//    atomManager.atoms().iterate(threeParticleLooper);
+//    atomManager.ghostAtoms().iterate(threeParticleLooper);
+//    return;
+
+    atomManager.atoms().iterate([&](Atom &atom1) {
+        atomManager.atoms().iterate([&](Atom &atom2) {
+            atomManager.atoms().iterate([&](Atom &atom3) {
+                if(atom1.originalUniqueId() < atom2.originalUniqueId() && atom1.originalUniqueId() < atom3.uniqueId() && atom2.uniqueId() < atom3.uniqueId()) m_threeParticleAction(&atom1,&atom2,&atom3);
+            });
+        });
+
+        atomManager.atoms().iterate([&](Atom &atom2) {
+            atomManager.ghostAtoms().iterate([&](Atom &atom3) {
+                if(atom1.originalUniqueId() < atom2.originalUniqueId() && atom1.originalUniqueId() < atom3.uniqueId() && atom2.uniqueId() < atom3.uniqueId()) m_threeParticleAction(&atom1,&atom2,&atom3);
+            });
+        });
+
+        atomManager.ghostAtoms().iterate([&](Atom &atom2) {
+            atomManager.ghostAtoms().iterate([&](Atom &atom3) {
+                if(atom1.originalUniqueId() < atom2.originalUniqueId() && atom1.originalUniqueId() < atom3.uniqueId() && atom2.uniqueId() < atom3.uniqueId()) m_threeParticleAction(&atom1,&atom2,&atom3);
+            });
+        });
     });
 }
